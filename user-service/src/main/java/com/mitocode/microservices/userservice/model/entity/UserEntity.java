797c84c6c440445b3d3d0484dd95c74724ae1d4b
MabMab^^ <miguelberrioh@hotmail.com>
2024-01-29 09:02:29 -0500

    Configuracion de user

--- user-service/src/main/java/com/mitocode/microservices/userservice/model/entity/UserEntity.java
+++ user-service/src/main/java/com/mitocode/microservices/userservice/model/entity/UserEntity.java
@@ -1,2 +1,20 @@
-package com.mitocode.microservices.userservice.model.entity;public class UserEntity {
+package com.mitocode.microservices.userservice.model.entity;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document("user")
+@Data //=> @Getter, @Setter, @EqualsAndHashCode, @ToString, @Value
+@Builder
+public class UserEntity {
+    @Id
+    private String id;
+    private String name;
+    private String lastname;
+    private String username;
+    private String email;
+    private String password;
+    private String[] roles;
 }
--- user-service/src/main/java/com/mitocode/microservices/userservice/repository/UserRepository.java
+++ user-service/src/main/java/com/mitocode/microservices/userservice/repository/UserRepository.java
@@ -1,2 +1,9 @@
-package com.mitocode.microservices.userservice.repository;public class UserRepository {
+package com.mitocode.microservices.userservice.repository;
+
+import com.mitocode.microservices.userservice.model.entity.UserEntity;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+@RepositoryRestResource(path = "user")
+public interface UserRepository extends CrudRepository<UserEntity, String> {
 }
